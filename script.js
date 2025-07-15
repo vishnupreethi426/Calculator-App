3e553c1c3c1082b3e5787f8f685478e0078aa756
vishnupreethi426 <vishnupreethib.24cse@kongu.edu>
2025-07-15 21:16:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  if (display.innerText === '0' || display.innerText === 'Error') {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = '0';
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch {
+    display.innerText = 'Error';
+  }
+}
